e9353b37d978653b541ca5ed0fd63e59c893889a
Jogeshwar Singh <singhjogeshwar007@gmail.com>
2025-08-03 14:28:01 +0530

    setup actix api and tests

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::net::TcpListener;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+
+    println!("http://127.0.0.1:{}", port);
+
+    zero2prod::run(listener)?.await
+}
